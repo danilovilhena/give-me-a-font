acd5877f1d62abdfea677ee9891ad4de07c7bcdd
Danilo Vilhena <danilo.vilhena@dellead.com>
2021-02-27 22:12:53 -0300

    Sans-serif and JSONs

--- src/components/ButtonPrimary.js
+++ src/components/ButtonPrimary.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-const ButtonPrimary = ({children}) => {
+const ButtonPrimary = (props) => {
     return (
-        <button className="text-2.5xl font-bold text-white border-4 border-purple-700 hover:border-purple-600 bg-purple-700 hover:bg-purple-600 transition duration-300 py-2 px-8 m-4 rounded-lg">
-            {children}
+        <button click={props.click} className="text-2.5xl font-bold text-white border-4 border-purple-700 hover:border-purple-600 bg-purple-700 hover:bg-purple-600 transition duration-300 py-2 px-8 m-4 rounded-lg">
+            {props.children}
         </button>
     )
 }
--- src/pages/sans-serif.js
+++ src/pages/sans-serif.js
@@ -1,43 +1,105 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import ButtonPrimary from "../components/ButtonPrimary"
 import ButtonSecondary from "../components/ButtonSecondary"
+import SansSerifJson from "../assets/sans-serif.json"
+
+const buildFontAndArrayString = (font) => {
+    let str = "https://fonts.googleapis.com/css2?family=" + encodeURIComponent(font.family)
+    let arr = [[],[]]
+    if(Object.keys(font.files).toString().includes('italic')){
+        str += ":ital,wght@"
+        Object.keys(font.files).forEach((file) => {
+            if(file === 'regular') arr[0].push('400')
+            else if(file === 'italic') arr[1].push('400')
+            else {
+                if(file.includes('italic')) arr[1].push(file.replace(/\D/g, ""))
+                else arr[0].push(file)
+            }
+        })
+        arr[0].sort(function(a, b) { return a - b })
+        arr[1].sort(function(a, b) { return a - b })
+
+        arr[0].forEach(el => {
+            str += "0," + el + ";"
+        })
+
+        arr[1].forEach(el => {
+            str += "1,"+ el + ";"
+        })
+
+        str = str.slice(0, -1)
+    } else {
+        str += ":wght@"
+        Object.keys(font.files)[Object.keys(font.files).indexOf('regular')] = '400'
+        Object.keys(font.files).forEach((file) => {
+            if(file === 'regular') arr[0].push(400)
+            else arr[0].push(file)
+        })
+        arr[0].map(el => { return +el })
+        arr[0].sort(function(a, b) { return a - b })
+        str += arr[0].toString().replaceAll(',', ';')
+    }
+
+    str += "&display=swap"
+
+    return str, arr
+}
 
 const SansSerifPage = () => {
+    const selectedFont = SansSerifJson.items[Math.floor(Math.random() * SansSerifJson.items.length)]
+    selectedFont['string', 'arr'] = buildFontAndArrayString(selectedFont)
+
     return (
     <Layout>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"></link>
+        <link href={selectedFont.string} rel="stylesheet"></link>
 
         <main className="max-w-6xl w-4/5 mx-auto flex flex-col">
-            <section className="flex flex-row justify-between align-start mb-12">
+            <section className="flex flex-row justify-between items-center mb-12">
                 <div>
-                    <h2>Here is your font: </h2>
-                    <p className="mt-2 text-purple-700 text-5xl" style={{fontFamily: ['Pacifico', 'display']}}>Pacifico</p>
+                    <h2>Here is your font:</h2>
+                    <p className="mt-2 text-purple-700 text-4xl" style={{fontFamily: [selectedFont.family, 'sans-serif']}}>{selectedFont.family}</p>
                 </div>
-                <div>
-                    <button className="text-xl font-bold text-white border-4 border-blue-600 hover:border-blue-500 bg-blue-600 hover:bg-blue-500 transition duration-300 py-2 px-4 m-4 rounded-lg">
+                <div className="mt-4">
+                    <a className="text-xl font-bold text-white border-4 border-blue-600 hover:border-blue-500 bg-blue-600 hover:bg-blue-500 transition duration-300 py-2 px-4 rounded-lg" href={"https://fonts.google.com/specimen/" + encodeURIComponent(selectedFont.family)}>
                         Google Fonts
-                    </button>
+                    </a>
                 </div>
             </section>
             <section className="mb-12">
                 <h2 className="mb-2">Font weights available:</h2>
-                <div className="flex">
-                    <div className="mr-12">
-                        <h3 className="text-2xl">Regular 400</h3>
-                        <p className="text-purple-700 text-5xl" style={{fontFamily: ['Pacifico', 'display']}}>Pacifico</p>
+                <div className="flex flex-wrap flex-col mb-4">
+                    <h3 className="text-3xl mb-2 font-bold">Regular</h3>
+                    <div className="flex flex-wrap justify-start">
+                        {selectedFont.arr[0].map((variant) => {
+                            return (
+                                <div className="mr-12" key={variant}>
+                                    <h4 className="text-2xl">{variant}</h4>
+                                    <p className="text-purple-700 text-3xl" style={{fontFamily: [selectedFont.family, 'sans-serif'], fontWeight: variant, fontStyle: 'normal'}}>{selectedFont.family}</p>
+                                </div>
+                            )
+                        })}
                     </div>
-                    <div>
-                        <h3 className="text-2xl">Bold 700</h3>
-                        <p className="text-purple-700 text-5xl" style={{fontFamily: ['Pacifico', 'display']}}>Pacifico</p>
+                </div>
+                <div className="flex flex-wrap flex-col">
+                    {selectedFont.arr[1].length !== 0 && <h3 className="text-3xl mb-2 font-bold">Italic</h3>}
+                    <div className="flex flex-wrap justify-start">
+                        {selectedFont.arr[1].length !== 0 && selectedFont.arr[1].map((variant) => {
+                            return (
+                                <div className="mr-12" key={variant}>
+                                    <h4 className="text-2xl">{variant}</h4>
+                                    <p className="text-purple-700 text-3xl" style={{fontFamily: [selectedFont.family, 'sans-serif'], fontWeight: variant, fontStyle: 'italic'}}>{selectedFont.family}</p>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
             <section className="flex items-center justify-center">
                 <Link to="/"><ButtonSecondary>Return</ButtonSecondary></Link>
-                <ButtonPrimary onClick={() => window.location.reload()}>Generate another</ButtonPrimary>
+                <Link to="/sans-serif"><ButtonPrimary>Generate another</ButtonPrimary></Link>
             </section>
         </main>
     </Layout>
