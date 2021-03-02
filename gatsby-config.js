06c1bdffcc3c2c013707026cbd425da5cb7b6835
Danilo Vilhena <danilo.vilhena@dellead.com>
2021-03-02 17:19:31 -0300

    Ajuste de URL base

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Give me a font!`,
     description: `Just pick a font style and we will give you a font based in that choice.`,
     author: `Danilo Vilhena`,
-    siteUrl: 'https://danilovilhena.com'
+    siteUrl: 'https://danilovilhena.github.io/give-me-a-font/'
   },
   plugins: [
     "gatsby-plugin-sitemap",
@@ -15,7 +15,7 @@ module.exports = {
       resolve: "gatsby-plugin-manifest",
       options: {
         name: "give-me-a-font",
-        start_url: "/",
+        start_url: "/give-me-a-font/",
         background_color: "#fff",
         theme_color: "#fff",
         display: "minimal-ui",
